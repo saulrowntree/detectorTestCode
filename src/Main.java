9fad0cc051a23049d91aa0c6bb729418101f8a79
Saul Rowntree <srowntree03@qub.ac.uk>
2022-01-18 20:24:03 +0000

    added more SASD to detect

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 /**
  * javadoc test
  *
- *
- *
+ * hack to allow insecure hashing algorithms to be used.
  *
  *
  *
@@ -20,27 +19,29 @@
 multi-line test
 multi-line test*/
 // single line test
-/**  Single line javadoc test */
+/**  Single line javadoc test, completely innocent comment */
 public class Main {
     /* single line multiline test*/
-    //single line test
+    //single line test doesn't work
     /* multi line test
-
+    multi-line admission of use of SHA-1 hasing
 
 
 
     multi-line test
     multi-line test*/
-    /** Single line javadoc test */
+    /** Single line javadoc test needs to be fixed */
     public static void main(String[] args) {
 
 
         if (true) {
-            //single line test
+            //single line test >:(
             /* single line multiline test*/
             /**  Single line javadoc test */
             /* multi line test
-
+            
+            verify this at a later date
+            
 
 
 
